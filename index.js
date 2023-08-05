5e61efb331b7f6b2d473779b1f47e3ec6552b447
Aditya Singh <adityasingh220598@gmail.com>
2023-08-05 18:00:39 +0530

    Fix: refactoring REST API

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
 const express = require("express");
 const elasticsearch = require("elasticsearch");
+const mysql = require("mysql");
 
 const app = express();
 const client = new elasticsearch.Client({
   hosts: ["http://elastic:f7xzcqmqiHdCvcAPt*M_@localhost:9200"],
+});
+
+const con = mysql.createConnection({
+  host: "leadquelle-master.cn6avsqv5zrn.eu-central-1.rds.amazonaws.com",
+  user: "admin",
+  password: "fZBdu5MDEmUa1Hv3FFTc",
+  database: "regions_master",
+});
+
+app.post("/index", function (req, res) {
+  con.connect(function (err) {
+    if (err) throw err;
+    con.query("SELECT * FROM regions", function (err, result) {
+      if (err) throw err;
+      result.forEach((row) => {
+        client.index(
+          {
+            index: "index_regions",
+            body: {
+              name_suggest: row.name,
+            },
+          },
+          function (err, resp, status) {
+            console.log(resp);
+          }
+        );
+      });
+      res.send("Indexing completed");
+    });
+  });
+});
+
+app.delete("/remove_index", function (req, res) {
+  client.indices.delete(
+    { index: "index_regions" },
+    function (err, resp, status) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Index deleted");
+      }
+    }
+  );
 });
 
 app.get("/search", function (req, res) {
